a2b7215e37bca000659a8c44fe9b15bbb190ff8c
ethanbonsall <ethanbonsall50@gmail.com>
2025-08-25 20:59:41 -0400

    Initial commit

--- web/components/nameLogo.tsx
+++
@@ -1,7 +0,0 @@
-export default function nameLogo() {
-  return (
-    <div className="px-1 py-[4px] md:px-2 md:py-[10px] items-center rounded-xl justify-items-center border-2 text-sans font-extrabold text-2xl md:text-3xl lg:text-4xl">
-      <a href="#home">EB</a>
-    </div>
-  );
-}
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,22 +1,9 @@
-import Courses from "../components/Portfolio/classes";
-import Header from "../components/Portfolio/headerNew";
-import Web from "../components/Portfolio/projects";
-import Education from "../components/Portfolio/education";
-import Skills from "../components/Portfolio/skills";
-import Experience from "../components/Portfolio/experience";
-import NavBar from "../components/Portfolio/navbar";
+
 
 const Portfolio = () => {
   return (
     <div className="bg-background flex flex-col items-center font-roboto min-h-screen">
-      <div className="h-6 flex w-full bg-accent" id="home"></div>
-      <NavBar />
-      <Header />
-      <Education />
-      <Courses />
-      <Web />
-      <Experience />
-      <Skills />
+      <div className="h-6 flex w-full bg-accent">Test</div>
     </div>
   );
 };
