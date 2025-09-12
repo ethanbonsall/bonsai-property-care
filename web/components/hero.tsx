7fda2898f43daca5654a851354d62ac4be247f96
ethanbonsall <ethanbonsall50@gmail.com>
2025-09-11 22:54:18 -0400

    Fixed link

--- web/components/hero.tsx
+++ web/components/hero.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import generic from "@/public/assets/generic.jpeg";
 import { ChevronRight } from "lucide-react";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -16,12 +17,12 @@ const Hero = () => {
       <p className=" text-white font-bold text-4xl md:text-5xl lg:text-6xl absolute top-[53dvh] md:top-[55dvh] lg:top-[53dvh] self-center left-[10dvw] md:left-[15dvw]">
         Simplify your life.
       </p>
-      <a
+      <Link
         className="flex flex-row p-2 md:p-3 bg-primary absolute text-text-50 top-[62dvh] md:top-[68dvh] lg:top-[62dvh] font-bold left-[10dvw] md:left-[15dvw] text-xl lg:text-2xl rounded-[4px]"
         href="/services"
       >
         Schedule Now <ChevronRight className="h-7 lg:h-9 w-auto" />
-      </a>
+      </Link>
     </div>
   );
 };
