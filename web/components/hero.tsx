37a2dee27ae155fd9abbef71ea9b93026dfdc96d
ethanbonsall <ethanbonsall50@gmail.com>
2025-09-14 15:07:31 -0400

    Image resizing

--- web/components/hero.tsx
+++ web/components/hero.tsx
@@ -5,14 +5,14 @@ const Hero = () => {
   return (
     <div className="flex flex-col min-h-screen md:h-[110dvh] w-full bg-cover bg-top md:bg-center bg-[url('/assets/generic.jpeg')] ">
       <div className="flex flex-col mx-auto md:mx-[15%] md:my-[5%] gap-y-2 justify-center align-middle min-h-screen md:h-[110dvh]">
-        <p className="flex text-white font-bold text-4xl md:text-5xl lg:text-6xl">
+        <p className="flex text-white font-bold text-5xl  lg:text-6xl">
           Brighten your home
         </p>
-        <p className="flex text-white font-bold text-4xl md:text-5xl lg:text-6xl">
+        <p className="flex text-white font-bold text-5xl lg:text-6xl">
           Simplify your life.
         </p>
         <Link
-          className="flex flex-row p-2 md:p-3 my-2 md:my-3 bg-primary text-text-50 text-xl lg:text-2xl font-semibold rounded-[4px] w-fit"
+          className="flex flex-row p-2 md:p-3 my-2 md:my-3 bg-primary text-text-50 text-2xl font-semibold rounded-[4px] w-fit"
           href="/services"
         >
           Schedule Now <ChevronRight className="h-7 lg:h-9 w-auto" />
