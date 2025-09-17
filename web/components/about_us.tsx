cf4591dd6b30471e025844dbb9aeb25e21bba2e4
ethanbonsall <ethanbonsall50@gmail.com>
2025-09-17 19:25:04 -0400

    Fix for method mobile

--- web/components/about_us.tsx
+++ web/components/about_us.tsx
@@ -52,7 +52,7 @@ const About = () => {
           } `}
         />
       </div>
-      <div className="flex flex-col justify-center mx-[2%] 2xl:gap-y-8">
+      <div className="flex flex-col justify-center mx-[2%] mt-[2%] lg:mt-0 2xl:gap-y-8">
         <p
           className={`font-bold text-6xl xl:text-[80px] 2xl:text-[150px] transition-all duration-700 ease-out ${
             isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"
