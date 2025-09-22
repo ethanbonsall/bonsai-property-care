cbc66620dc50cd1a4f3125f40a731ff388dfdbe4
ethanbonsall <ethanbonsall50@gmail.com>
2025-09-22 13:39:43 -0400

    Make contact functional

--- web/components/contact.tsx
+++ web/components/contact.tsx
@@ -1,4 +1,46 @@
+import { useState } from "react";
+import emailjs from "emailjs-com";
+
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Message, setMessage] = useState("");
+  const Address = "Contact";
+  const date = "Contact";
+  const Service = "Contact";
+
+  const handleSend = async () => {
+    if (!name || !Email || !Message) {
+      alert("Please fill in all parts of the form.");
+      return;
+    }
+
+    try {
+      await emailjs.send(
+        "service_4g72ymf",
+        "template_r3isus9",
+        {
+          name,
+          Number,
+          Address,
+          Message,
+          date,
+          Email,
+          Service,
+        },
+        "uhdisYswgUANDPxsU"
+      );
+
+      alert("Message sent successfully!");
+      setName("");
+      setEmail("");
+      setMessage("");
+    } catch (error) {
+      console.error("Error during form submission:", error);
+      alert("Something went wrong. Please try again.");
+    }
+  };
+
   return (
     <div className="flex flex-col bg-gradient-to-bl from-primary p-8 lg:p-16 to-secondary justify-center items-center text-background min-h-[75dvh] w-full">
       <div className="flex flex-col text-left gap-y-2 mb-16">
@@ -17,6 +59,7 @@ const Contact = () => {
               autoComplete="Name"
               type="text"
               name="Name"
+              onInput={(e) => setName((e.target as HTMLInputElement).value)}
             ></input>
           </div>
           <div className="flex flex-col">
@@ -26,6 +69,7 @@ const Contact = () => {
               autoComplete="email"
               type="text"
               name="Email"
+              onInput={(e) => setEmail((e.target as HTMLInputElement).value)}
             ></input>
           </div>
         </div>
@@ -35,9 +79,13 @@ const Contact = () => {
             <textarea
               className="p-2 rounded text-text bg-background w-[80dvw] lg:w-[675px] border shadow"
               name="message"
+              onInput={(e) => setMessage((e.target as HTMLInputElement).value)}
             ></textarea>
           </div>
-          <button className="text-background bg-primary text-xl shadow px-4 py-2 rounded">
+          <button
+            className="text-background bg-primary text-xl shadow px-4 py-2 rounded"
+            onClick={handleSend}
+          >
             Submit
           </button>
         </div>
